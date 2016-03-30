d80608aec5437a8d0450700341c64515432d51db
Jeff <200006506@qq.com>
2016-03-30 16:45:00 +0800

    add api doc

--- api.py
+++ api.py
@@ -25,51 +25,78 @@ class ModelResource(Resource):
             allow_delete = True
     """
     # 模型
+    # type: sqlalchemy obj 需要继承 CRUDMixin
     model = None
 
     # 创建数据使用的表单
+    # type: wtforms obj
     create_form = None
 
     # 更新数据使用的表单
+    # type: wtforms obj
     update_form = None
 
     # 输出格式定义
+    # type: flask_restful fields obj
     output_fields = None
 
     # 是否允许创建
+    # type: bool
     allow_create = False
 
     # 是否允许更新
+    # type: bool
     allow_update = False
 
     # 是否允许删除
+    # type: bool
     allow_delete = False
 
     # 页码
+    # type: int
     page = 1
 
     # 每页数据个数
+    # type: int
     per_page = 20
 
     # 是否生成包含域名的完整url
+    # type: bool
     is_full_url = False
 
+    # 自定义排序
+    # type: list or tuple or set
     order_by = None
 
     @property
     def cls_name(self):
+        """ 视图类名称
+        :return: str
+        """
         return self.__class__.__name__
 
     def check_model(self):
+        """ 检查模型是否设置
+        :return:
+        """
         assert self.model, 'API Class: {} model not set'.format(self.cls_name)
 
     def check_create_form(self):
+        """ 检查创建表单是否设置
+        :return:
+        """
         assert self.create_form, 'API Class: {} create_form not set'.format(self.cls_name)
 
     def check_update_form(self):
+        """ 检查更新表单是否设置
+        :return:
+        """
         assert self.update_form, 'API Class: {} update_form not set'.format(self.cls_name)
 
     def check_output_fields(self):
+        """ 检查输出格式是否设置
+        :return:
+        """
         assert self.output_fields, 'API Class: {} output_fields not set'.format(self.cls_name)
 
     @property
