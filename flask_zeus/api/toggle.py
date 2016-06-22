13c5fb4d48ed395687d2c577111d7c92c5de1938
Jeff <200006506@qq.com>
2016-06-22 11:03:36 +0800

    add toggle api form

--- flask_zeus/api/toggle.py
+++ flask_zeus/api/toggle.py
@@ -6,9 +6,10 @@ from .errors import *
 
 class ToggleApi(RestfulApi):
 
+    form = None
+
     @login_required
     def post(self, **kwargs):
-
         if not self.model.has_property('user_id'):
             raise ZeusMethodNotAllowed
 
@@ -16,9 +17,17 @@ class ToggleApi(RestfulApi):
         item = stmt.first()
 
         if not item:
+            data = kwargs
+
+            if self.form:
+                form = self.get_form(self.form, **kwargs)
+                if not form.validate_on_submit():
+                    raise ZeusBadRequest(details=form.errors)
+                data = form.data
+
             model = self.get_model()
             item = model()
-            for k, v in kwargs.items():
+            for k, v in data.items():
                 if model.has_property(k):
                     setattr(item, k, v)
             item.user_id = current_user.id
